0bbcbc19dacdc2433ac60a58e343eb242c7ff4df
Elva-lab <elva.hamiti@gmail.com>
2024-10-14 16:07:03 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@variablat/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() { 
+  cout << "Variablat!\n"; 
+  int x=5;
+  int y=7;
+  int z=x+y;
+  cout << "x=" << x << endl;
+  cout << "y=" << y << endl;
+  cout << "z=" << z << endl;
+  
+
+}
